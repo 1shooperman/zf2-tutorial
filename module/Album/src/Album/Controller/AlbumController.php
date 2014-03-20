8f54a5bf587e51ba840e3ceebad36cbeac0bb85d
bshoop117 <brandon.shoop@dominionmarinemedia.com>
2014-03-20 16:48:35 -0400

    adding album module

---
+++ module/Album/src/Album/Controller/AlbumController.php
@@ -0,0 +1,24 @@
+<?php
+namespace Album\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class AlbumController extends AbstractActionController  /** also @see AbstractRestfulController */
+{
+    public function indexAction()
+    {
+    }
+
+    public function addAction()
+    {
+    }
+
+    public function editAction()
+    {
+    }
+
+    public function deleteAction()
+    {
+    }
+}
